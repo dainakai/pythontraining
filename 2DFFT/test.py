0935833da1382f9cc1f94cd1a28e0afc85b5847e
dainakai <dainakai1031@gmail.com>
2021-04-15 20:00:21 +0900

    a

--- 2DFFT/test.py
+++ 2DFFT/test.py
@@ -1,36 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import cv2
 
-N = 512
-T_LEN = 1.0
+img = cv2.imread("unnamed.jpg",1)
 
-volt = np.zeros(N)
-time = np.zeros(N)
-for i in range(N):
-    volt[i] = np.sin(2*np.pi*1*i/N) + np.sin(2*np.pi*3*i/N) + np.sin(2*np.pi*6*i/N)
-    time[i] = i*T_LEN/N
-
-plt.plot(time,volt,label = "time series")
-plt.legend()
-plt.show()
-
-data = np.fft.fft(volt)
-
-freq = np.zeros(N)
-for i in range(N):
-    freq[i] = i/T_LEN
-
-data = abs(data)**2
-
-plt.plot(freq,data,label = "Frequency")
-# plt.xlim(0,10)
-plt.legend()
-plt.show()
-
-
-
-
-
-
-
-
+cv2.imshow("test",img)
+cv2.waitKey(0)
+cv2.destroyAllwindows()
